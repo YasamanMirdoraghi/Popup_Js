5ed71889021f6e392d4c205410be3be1cdf20ae9
Yasaman <125014861+YasamanMirdoraghi@users.noreply.github.com>
2023-03-04 09:32:28 +0330

    Add files via upload

---
+++ js/pop.js
@@ -0,0 +1,10 @@
+
+
+
+
+document.getElementById("btt").addEventListener("click", function(){
+    document.querySelector('.pop').style.display="flex"
+})
+document.getElementById("_bttu").addEventListener("click", function(){
+    document.querySelector('.pop').style.display="none"
+})
